0882f5cd24ff28e2baa63df1de7f55f587957e57
Glitch (hello-webpage) <none>
2020-12-06 21:11:10 +0000

    💏🛍 Checkpoint ./index.html:22072682/424 ./script.js:22072682/603 ./time.js:22072682/520 ./game.js:22072682/1628 ./level.js:22072682/620 ./images.js:22072682/1226

---
+++ level.js
@@ -0,0 +1,28 @@
+let level = 0;
+const levelNumber = 3;
+
+
+$(document).ready(function() {
+  loadLevels();
+});
+
+function loadLevels() {
+  for (let i = 0; i < levelNumber; i++) {
+    let button = document.createElement("button");
+     button.setAttribute("type", "button")
+    button.classList.add("btn", "mx-1");
+    button.id = 1+i
+    button.textContent = "Level " + (1+i)
+    button.setAttribute("onclick", "levelClicked(this.id)")
+    document.getElementById("levels").appendChild(button)
+  }
+  
+}
+function levelClicked(levelId) {
+  console.log(levelId)
+  level = levelId;
+  totalSeconds = 0;
+  isPlaying = true
+  game.initialize();
+}
+
---
+++ time.js
@@ -0,0 +1,22 @@
+
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  if(isPlaying){
+    totalSeconds+=1;
+  }  
+  secondsLabel.innerHTML = convertString(totalSeconds % 60);
+  minutesLabel.innerHTML = convertString(parseInt(totalSeconds / 60));
+}
+
+function convertString(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
